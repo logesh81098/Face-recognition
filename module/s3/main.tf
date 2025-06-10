2c0b6aca9892dd2cd5151ba2b90976d57b66efd6
Logesh Shanmugavel <logesh81098@gmail.com>
2025-06-10 18:09:47 +0530

    Deployed S3 bucket to store source images

---
+++ module/s3/main.tf
@@ -0,0 +1,13 @@
+#######################################################################################################
+#                                             S3 Bucket
+#######################################################################################################
+
+#S3 bucket to store source images
+
+resource "aws_s3_bucket" "source-bucket" {
+  bucket = "face-rekognition-source-bucket"
+  tags = {
+    Name = "face-rekognition-source-bucket"
+    Project = "Face Recognition"
+  }
+}
