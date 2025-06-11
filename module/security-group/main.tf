6819ff66c04e09d9db27b08489ad1330fb3c6b95
Logesh Shanmugavel <logesh81098@gmail.com>
2025-06-11 14:24:37 +0530

    Deployed Security Group [Wiring component]

---
+++ module/security-group/main.tf
@@ -0,0 +1,34 @@
+##################################################################################################################################
+#                                                  Security Group
+##################################################################################################################################
+
+#Security Group for EC2 instance
+
+resource "aws_security_group" "Face-Recognition-SG" {
+  name = "Face-Recognition-SG"
+  vpc_id = var.vpc-id
+  ingress {
+    from_port = var.HTTPS-Port
+    to_port = var.HTTPS-Port
+    protocol = "tcp"
+    cidr_blocks = [var.anywhere-cidr]
+    description = "Ingress HTTPS connection from anywhere"
+  }
+  ingress {
+    from_port = var.SSH-Port
+    to_port = var.SSH-Port
+    protocol = "tcp"
+    cidr_blocks = [var.anywhere-cidr]
+    description = "Ingress SSH connection from anywhere"
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [var.anywhere-cidr]
+  }
+  tags = {
+    Name = "Face-Recognition-SG"
+    Project = "Face Recognition"
+  }
+}
---
+++ module/security-group/variable.tf
@@ -0,0 +1,15 @@
+variable "vpc-id" {
+  default = {}
+}
+
+variable "HTTPS-Port" {
+  default = "443"
+}
+
+variable "SSH-Port" {
+  default = "22"
+}
+
+variable "anywhere-cidr" {
+  default = "0.0.0.0/0"
+}
